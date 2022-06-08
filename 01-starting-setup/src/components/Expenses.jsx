691716c6d7f5011177b8d7ee339654c695bb8bad
Perry David Ralston Jr <ralstontech@gmail.com>
2022-06-08 16:32:37 -0700

    incremental commit

---
+++ 01-starting-setup/src/components/Expenses.jsx
@@ -0,0 +1,17 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  const expenseItems = props.expenses.map((expense) => (
+    <ExpenseItem
+      id={expense.id}
+      date={expense.date}
+      title={expense.title}
+      amount={expense.amount}
+    />
+  ));
+
+  return <div className="expenses">{expenseItems}</div>;
+}
+
+export default Expenses;
